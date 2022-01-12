9626a978255dd7b5615b72db1a83996877f7084b
koki2 <Dub1Sub2>
2022-01-12 12:22:19 +0200

    hw1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import './App.css';
+import Simpson from "./Components/Simpson";
 
 const App = () => {
     return (
         <div>
+
+            <Simpson name={`lisa`} age={11}/>
+            <Simpson name={`bart`} age={1111111}/>
+
+
 
         </div>
     );
---
+++ src/Components/Simpson.js
@@ -0,0 +1,9 @@
+export default function Simpson(props) {
+    let {name, age} = props;
+    return (
+        <div>
+            {name} {age}
+        </div>
+    )
+};
+
