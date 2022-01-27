876ae3004ef4beb0a5a42ace5d47e5be0adf5653
koki2 <Dub1Sub2>
2022-01-27 19:47:12 +0200

    4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 const App = () => {
 
+    const reducer = (state, action) => {
+        return state;
+    }
+
+    let [state, dispatch] = useReducer(reducer, {count1: 0});
 
     return (<>
     </>);
