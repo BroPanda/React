9469fb352a41fb9ff6aed70a29693848355a4b28
koki2 <Dub1Sub2>
2022-02-01 21:18:09 +0200

    bonus

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
+import {carService} from "../../services/car.service";
 
 
 const Form = () => {
-    const {register, handleSubmit, watch, reset,formState:{errors}} = useForm();
+    const {register, handleSubmit, watch, reset, formState: {errors}} = useForm();
+
+    const submit = (data) => {
+        // console.log(data);
+        carService.create(data).then(value => console.log(value)).catch(errors => console.log(errors.response.data));
+        reset();
+    }
+
+    // watch(e => console.log(e))
+
     return (
         <div>
-            <form>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
-
+            <form onSubmit={handleSubmit(submit)}>
+                <label> MODEL <input type="text" {...register('model',)}/></label>
+                <label> YEAR <input type="text" {...register('year',)}/></label>
+                <label> PRICE <input type="text" {...register('price',)}/></label>
+                <button>Save</button>
             </form>
         </div>
     );
